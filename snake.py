8d7f1f4a2241418dcc05c14b4a72172f7d0f06b3
girach <mdbilalgirach7@gmail.com>
2017-12-07 21:49:00 +0530

    Added background, music and removed debugging code

--- snake.py
+++ snake.py
@@ -9,7 +9,7 @@ class dude:
 	speed = 35
 	length =3
 	direction=0 # direction determiner
-	update_max_ctr=2 # A factor which determines the speed of the snake
+	update_max_ctr=0 # A factor which determines the speed of the snake
 	update_ctr =0
 	
 	def __init__(self,length):
@@ -17,7 +17,6 @@ class dude:
 		for i in range(0,2000): # Predefining snake's length 
 			self.x.append(-100)
 			self.y.append(-100)
-		#print(self.x)	
 		
 		# To prevent initial collision
 		self.x[1] = 1*40
@@ -27,7 +26,6 @@ class dude:
 		
 		self.update_ctr = self.update_ctr + 1
 		
-		#print("Here in update",self.update_ctr)
 		if self.update_ctr > self.update_max_ctr: # the more the max_ctr the slower the snake
  			
  			# Snake's head is at index 0;
@@ -84,8 +82,8 @@ def collision(x1,y1,x2,y2,bsize):
 	  
 class pygame_window:
 	
-	height = 600
-	width = 800
+	height = 1000
+	width = 1000
 	
 	def __init__(self):
 		self.running = True
@@ -93,22 +91,24 @@ class pygame_window:
 		self.image = None
 		self.player = dude(3) #Base length
 		self.apple = Apple(5,5)
-		#print("Helo")
 	
 	
 	def on_start(self):
 		pygame.init()
-		#print("Entered here")
+		pygame.mixer.init()
 		self.display = pygame.display.set_mode((self.width,self.height))
 		pygame.display.set_caption('First Snake game')
+		pygame.mixer.music.load('music.mp3')
+		pygame.mixer.music.play(-1, 0.0)
+		pygame.mixer.music.set_volume(0.5)
 		self.running = True
 		self.image = pygame.image.load("snake.png").convert() # convert() -- Make loading things faster..
 		self.apple_img = pygame.image.load("apple.png").convert()
-		#print("on_start_completed")
 			
 	def on_loop(self): # Updates the positions
         	
         	self.player.update()
+        	
         	# If snake collides with the apple
         	for i in range(self.player.length):
         		if collision(self.apple.x,self.apple.y,self.player.x[i], self.player.y[i],30):
@@ -128,6 +128,7 @@ class pygame_window:
         
 	def printer(self): # Fills the screen appropriately
 		self.display.fill((0,0,0))
+		self.display.blit(pygame.image.load("Back.png").convert(),(0,0))
 		self.player.draw(self.display, self.image)
 		self.apple.draw(self.display, self.apple_img)
 		pygame.display.flip();
@@ -142,7 +143,6 @@ class pygame_window:
 		while(self.running):
 			pygame.event.pump()
 			keys = pygame.key.get_pressed()
-			#print("inside while loop")
 			if (keys[K_RIGHT]):
 				self.player.move_right()
 				
@@ -153,7 +153,6 @@ class pygame_window:
 				self.player.move_up()
 			
 			if (keys[K_DOWN]):
-				#print("pressed down")
 				self.player.move_down()
 				
 			if (keys[K_ESCAPE]): # Quit the game
@@ -161,7 +160,7 @@ class pygame_window:
 			
 			self.on_loop()	
 			self.printer() 
-			time.sleep (50.0 / 1000.0); # Another factor which determines the speed of the snake.
+			time.sleep (25.0 / 1000.0); # Another factor which determines the speed of the snake.
 		
 		self.close() 
 
