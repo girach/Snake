cca691869e578ded9f2235c65b8e7e8398796b8a
girach <mdbilalgirach7@gmail.com>
2017-12-07 20:24:58 +0530

    Game logic introduced..Can be played

--- snake.py
+++ snake.py
@@ -1,29 +1,36 @@
 from pygame.locals import*
 import pygame
 import time
+import random
 
 class dude:
-	x=[] # List of snake segment's positions
-	y=[]
+	x=[0] 
+	y=[0]
 	speed = 35
+	length =3
 	direction=0 # direction determiner
 	update_max_ctr=2 # A factor which determines the speed of the snake
 	update_ctr =0
 	
 	def __init__(self,length):
 		self.length = length
-		self.x = [0]*self.length
-		self.y = [0]*self.length
+		for i in range(0,2000): # Predefining snake's length 
+			self.x.append(-100)
+			self.y.append(-100)
 		#print(self.x)	
 		
+		# To prevent initial collision
+		self.x[1] = 1*40
+		self.x[2] = 2*40
+		
 	def update(self):
-	
-	# Snake's head is at index 0;
-	
+		
 		self.update_ctr = self.update_ctr + 1
+		
 		#print("Here in update",self.update_ctr)
 		if self.update_ctr > self.update_max_ctr: # the more the max_ctr the slower the snake
- 
+ 			
+ 			# Snake's head is at index 0;
             # Previous positions
 			for i in range(self.length-1,0,-1):
 				self.x[i] = self.x[i-1]
@@ -40,6 +47,7 @@ class dude:
 				self.y[0] = self.y[0] + self.speed
  
 			self.update_ctr = 0
+	
 	# Movement methods
 	def move_right(self):	
 		self.direction = 0
@@ -58,7 +66,7 @@ class Apple:
 	
 	x=0
 	y=0
-	speed = 44
+	speed = 35
 	
 	def __init__(self,x,y):
 		self.x = x * self.speed
@@ -66,6 +74,14 @@ class Apple:
 	
 	def draw(self, surface, image):
 		surface.blit(image,(self.x, self.y))
+
+def collision(x1,y1,x2,y2,bsize):
+
+	if x1>=x2 and x1<=x2+bsize:
+		if y1>=y2 and y1<=y2+bsize:
+			return True
+	return False
+	  
 class pygame_window:
 	
 	height = 600
@@ -75,9 +91,10 @@ class pygame_window:
 		self.running = True
 		self.display = None
 		self.image = None
-		self.player = dude(8) #Base length
+		self.player = dude(3) #Base length
 		self.apple = Apple(5,5)
 		#print("Helo")
+	
 	
 	def on_start(self):
 		pygame.init()
@@ -90,8 +107,24 @@ class pygame_window:
 		#print("on_start_completed")
 			
 	def on_loop(self): # Updates the positions
+        	
         	self.player.update()
-        	pass
+        	# If snake collides with the apple
+        	for i in range(self.player.length):
+        		if collision(self.apple.x,self.apple.y,self.player.x[i], self.player.y[i],30):
+		            self.apple.x = random.randint(2,9) * 35
+		            self.apple.y = random.randint(2,9) * 35
+		            self.player.length = self.player.length + 1
+			
+		    # If snake touches itself
+			for i in range(2,self.player.length):
+				if collision(self.player.x[0],self.player.y[0],self.player.x[i], self.player.y[i],30):
+					print("You lose! Collision: ")
+					print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
+					print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + str(self.player.y[i]) + ")")
+					exit(0)
+		 
+	    	pass
         
 	def printer(self): # Fills the screen appropriately
 		self.display.fill((0,0,0))
@@ -128,7 +161,8 @@ class pygame_window:
 			
 			self.on_loop()	
 			self.printer() 
-			time.sleep (100.0 / 1000.0); # Another factor which determines the speed of the snake.
+			time.sleep (50.0 / 1000.0); # Another factor which determines the speed of the snake.
+		
 		self.close() 
 
 if __name__ == "__main__":
